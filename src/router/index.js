accb0ddf2ba82a2bf5ddc4e2410fe96897bef237
DuHuaL <469580396@qq.com>
2020-05-28 15:40:46 +0800

    登录页面搭建

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-
+import Login from '@/views/login';
 Vue.use(Router);
 
 export default new Router({
   routes: [
+    { name: 'l', path: '/', redirect: {name: 'login'} },
+    { name: 'login', path: '/login', component: Login }
   ]
 });
---
+++ src/views/login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="login">
+    <el-form ref="form" :model="form" label-width="80px" class="login-form" label-position="top">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" class="login-btn">登 录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: '',
+        password: ''
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.login {
+  background-color: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-form {
+  width: 400px;
+  padding: 30px;
+  background-color: #fff;
+  border-radius: 5px;
+}
+.login-btn {
+  width: 100%;
+}
+</style>
