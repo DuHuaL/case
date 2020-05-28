0da9453b925adb700f71e829b0d33ad3070e607e
DuHuaL <469580396@qq.com>
2020-05-28 17:33:58 +0800

    修改登录功能的写法

--- src/views/login.vue
+++ src/views/login.vue
@@ -26,25 +26,16 @@ export default {
     };
   },
   methods: {
-    handleLogin() {
-      this.$http.post('login', this.form)
-        .then((res) => {
-          const data = res.data;
-          if (data.meta.status === 200) {
-            // 登录成功
-            // 跳转
-            // 提示
-            // 保存token
-            sessionStorage.setItem('token', data.data.token);
-            this.$message.success(data.meta.msg);
-          } else {
-            // 登录失败
-            this.$message.error(data.meta.msg);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+    async handleLogin() {
+      const res = await this.$http.post('login', this.form);
+      const data = res.data;
+      if (data.meta.status === 200) {
+        this.$message.success(data.meta.msg);
+        sessionStorage.setItem('token', data.data.token);
+        // 跳转到home
+      } else {
+        this.$message.error(data.meta.msg);
+      }
     }
   }
 };
